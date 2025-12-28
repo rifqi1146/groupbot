d74b39933b7bb93ed3525db71e5c819a51f5307d
rifqy1146 <rifqikunduran@gmail.com>
2025-12-28 19:22:04 +0700

    major update

--- utils/http.py
+++ utils/http.py
@@ -1,11 +1,20 @@
 import aiohttp
+import logging
 
-_session = None
+logger = logging.getLogger(__name__)
+
+_HTTP_SESSION: aiohttp.ClientSession | None = None
 
 async def get_http_session():
-    global _session
-    if _session is None or _session.closed:
-        _session = aiohttp.ClientSession(
-            timeout=aiohttp.ClientTimeout(total=30)
+    global _HTTP_SESSION
+    if _HTTP_SESSION is None or _HTTP_SESSION.closed:
+        _HTTP_SESSION = aiohttp.ClientSession(
+            timeout=aiohttp.ClientTimeout(total=60)
         )
-    return _session
+    return _HTTP_SESSION
+
+async def close_http_session():
+    global _HTTP_SESSION
+    if _HTTP_SESSION and not _HTTP_SESSION.closed:
+        await _HTTP_SESSION.close()
+        logger.info("HTTP session closed")
