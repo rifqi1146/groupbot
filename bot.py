65bf7fb22a1fe1e530d7879c5ecb8e9a8083540f
rifqy1146 <rifqikunduran@gmail.com>
2025-12-28 19:52:24 +0700

    update bot.py

--- bot.py
+++ bot.py
@@ -162,6 +162,38 @@ def setup_logger():
 
     return root
 
+#log 
+BOT_COMMANDS = {
+    "start",
+    "help",
+    "menu",
+    "helpowner",
+    "ask",
+    "weather",
+    "ping",
+    "enablensfw",
+    "disablensfw",
+    "nsfwlist",
+    "speedtest",
+    "ip",
+    "whoisdomain",
+    "domain",
+    "dl",
+    "stats",
+    "tr",
+    "gsearch",
+    "enableasupan",
+    "disableasupan",
+    "asupanlist",
+    "asupan",
+    "wlc",
+    "restart",
+    "ai",
+    "setmodeai",
+    "groq",
+    "nsfw",
+}
+
 # Dollar Command Router
 _DOLLAR_CMD_MAP = {
     "start": start_cmd,
