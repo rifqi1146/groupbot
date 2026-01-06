d9142c37a3022eda8f3b044c9cf9404902149705
rifqy1146 <rifqikunduran@gmail.com>
2026-01-06 10:45:45 +0700

    update major

---
+++ handlers/callbacks.py
@@ -0,0 +1,15 @@
+from telegram.ext import CallbackQueryHandler
+
+from handlers.help import help_callback
+from handlers.gsearch import gsearch_callback
+from handlers.dl import dl_callback, dlask_callback
+from handlers.asupan import asupan_callback
+from handlers.helpowner import helpowner_callback
+
+def register_callbacks(app):
+    app.add_handler(CallbackQueryHandler(help_callback, pattern=r"^help:"))
+    app.add_handler(CallbackQueryHandler(gsearch_callback, pattern=r"^gsearch:"))
+    app.add_handler(CallbackQueryHandler(dl_callback, pattern=r"^dl:"))
+    app.add_handler(CallbackQueryHandler(dlask_callback, pattern=r"^dlask:"))
+    app.add_handler(CallbackQueryHandler(asupan_callback, pattern=r"^asupan:"))
+    app.add_handler(CallbackQueryHandler(helpowner_callback, pattern=r"^helpowner:"))
