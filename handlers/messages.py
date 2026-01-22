b1f6c7b2d955d14a4c756b23f3eb1288d572283f
rifqy1146 <rifqikunduran@gmail.com>
2026-01-22 20:25:48 +0700

    adalah pokoknya

--- handlers/messages.py
+++ handlers/messages.py
@@ -18,18 +18,19 @@ async def ai_reply_router(update, context):
     if not msg or not msg.reply_to_message:
         return
 
+    user_id = msg.from_user.id
     chat_id = update.effective_chat.id
     reply_mid = msg.reply_to_message.message_id
-    
+
     if _ASK_ACTIVE_USERS.get(chat_id) == reply_mid:
         return await ask_cmd(update, context)
-        
+
     if _AI_ACTIVE_USERS.get(chat_id) == reply_mid:
         return await ai_cmd(update, context)
-        
+
     if _GROQ_ACTIVE_USERS.get(chat_id) == reply_mid:
         return await groq_query(update, context)
-        
+
     if _META_ACTIVE_USERS.get(user_id) == reply_mid:
         return await meta_query(update, context)
 
