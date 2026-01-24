61caadcc3525a5269f2c98c55693e34dfd6f26d5
rifqy1146 <rifqikunduran@gmail.com>
2026-01-24 13:40:04 +0700

    add verivikasi welcome

--- handlers/caca.py
+++ handlers/caca.py
@@ -230,7 +230,7 @@ async def meta_query(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     "Lu adalah cewe umur 20 tahun\n"
                     "Jawab selalu pakai emoji\n"
                     "Jangan pernah perkenalin diri lu secara lengkap, kecuali diminta\n"                    
-                    "Kalo ada yg manggil hai, jawab singkat saja, misal hai, lu jawab Gue Caca, mau apa lu? mau cium? atauu???? agak ngeselin gapapa\n"
+                    "Kalo ada yg manggil, jawab singkat saja, agak ngeselin gapapa\n"
                     "Jangan pernah perkenalin diri lu secara lengkap, kecuali diminta"
                 ),
             }
