51b84a8d3e0133a73b3acf55f41fdf7b59888883
rifqy1146 <rifqikunduran@gmail.com>
2026-01-27 21:30:33 +0700

    test music

--- handlers/dl.py
+++ handlers/dl.py
@@ -373,7 +373,12 @@ async def _dl_worker(app, chat_id, reply_to, raw_url, fmt_key, status_msg_id):
 
                     bot_name = (await bot.get_me()).first_name or "Bot"
                     fixed_audio = reencode_mp3(tmp_audio)
-
+                    
+                    await context.bot.send_chat_action(
+                        chat_id=chat_id,
+                        action="upload_audio"
+                    )
+                    
                     await bot.send_audio(
                         chat_id=chat_id,
                         audio=fixed_audio,
