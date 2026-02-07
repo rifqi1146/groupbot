ec1cbf2668760d57052cef9aecd50fdc63e35794
rifqy1146 <rifqikunduran@gmail.com>
2026-02-07 19:17:48 +0700

    update auto detect

--- handlers/join.py
+++ handlers/join.py
@@ -1,32 +1,41 @@
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, Update
 from telegram.ext import ContextTypes
 
-SUPPORT_GROUP_ID = -1003707701162
-SUPPORT_GROUP_LINK = "https://t.me/kiyoshibot"
+SUPPORT_CHANNEL_ID = -1003823718542
+SUPPORT_CHANNEL_LINK = "https://t.me/kiyoshi_support"
 
-async def is_joined_support_group(user_id: int, context: ContextTypes.DEFAULT_TYPE) -> bool:
+
+async def is_joined_support_channel(
+    user_id: int,
+    context: ContextTypes.DEFAULT_TYPE
+) -> bool:
     try:
         member = await context.bot.get_chat_member(
-            SUPPORT_GROUP_ID,
+            SUPPORT_CHANNEL_ID,
             user_id
         )
         return member.status in ("member", "administrator", "creator")
 
     except Exception as e:
         print("[JOIN CHECK ERROR]", e)
-        return False   # ⬅️ INI FIX UTAMA
+        return False
+
 
 def join_required_keyboard():
     return InlineKeyboardMarkup([
         [
             InlineKeyboardButton(
-                "Join Support Group",
-                url=SUPPORT_GROUP_LINK
+                "Join Support Channel",
+                url=SUPPORT_CHANNEL_LINK
             )
         ]
     ])
 
-async def require_join_or_block(update: Update, context: ContextTypes.DEFAULT_TYPE) -> bool:
+
+async def require_join_or_block(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+) -> bool:
     if update.callback_query:
         user = update.callback_query.from_user
         reply_target = update.callback_query.message
@@ -39,20 +48,20 @@ async def require_join_or_block(update: Update, context: ContextTypes.DEFAULT_TY
     if not user:
         return False
 
-    joined = await is_joined_support_group(user.id, context)
+    joined = await is_joined_support_channel(user.id, context)
     if joined:
         return True
 
     text = (
         "<b>Untuk menggunakan fitur download</b>\n\n"
-        "kamu wajib join dulu ke grup support.\n\n"
-        "📢 <b>Support Group</b>\n"
+        "kamu wajib join dulu ke channel support.\n\n"
+        "📢 <b>Support Channel</b>\n"
     )
 
     try:
         if update.callback_query:
             await update.callback_query.answer(
-                "Join dulu grup support ya 👀",
+                "Join dulu channel support ya 👀",
                 show_alert=True
             )
 
