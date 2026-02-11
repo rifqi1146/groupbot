6305249457e3a5d96af06d867a8cdb145c19e6a9
rifqy1146 <rifqikunduran@gmail.com>
2026-02-11 16:42:47 +0700

    update asupan

--- handlers/asupan.py
+++ handlers/asupan.py
@@ -296,6 +296,7 @@ async def fetch_asupan_tikwm(keyword: str | None = None):
     default_keywords = [
         "tante holic",
         "araasshr",
+        "tataaasiuu20",
         "nasluvt",
         "dimpledtataww",
         "lvme4awaa",
