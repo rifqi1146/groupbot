1c26e2d245421f0154d5d3e9aa2d386d99b99ad3
rifqy1146 <rifqikunduran@gmail.com>
2026-04-09 20:18:44 +0700

    trandlate bot.py

--- bot.py
+++ bot.py
@@ -115,7 +115,7 @@ async def post_init(app):
             ("ship", "Choose couple"),
             ("stats", "System statistics"),
             ("dl", "Download video"),
-            ("manga", "Baca Manga"),
+            ("manga", "Read Manga"),
             ("ask", "Ask Gemini AI"),
             ("music", "Search music"),
             ("caca", "Chat sama caca 😍"),
