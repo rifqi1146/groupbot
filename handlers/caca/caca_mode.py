e87287008fbac04f7e2d70e267e465f776c8c9ea
rifqy1146 <rifqikunduran@gmail.com>
2026-04-19 18:28:51 +0700

    refactor premium module into single cached database service

--- handlers/caca/caca_mode.py
+++ handlers/caca/caca_mode.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 from .caca_prompt import PERSONAS
-from database import premium_service
+from database import premium
 from database import caca_db
 from utils import caca_memory
 
