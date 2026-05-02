1c7bb29dd11a863a13dfcbfd3c86bc7e531bd035
rifqy1146 <rifqikunduran@gmail.com>
2026-05-02 21:06:31 +0700

    Add member tag moderation commands

--- handlers/moderation/__init__.py
+++ handlers/moderation/__init__.py
@@ -1,6 +1,6 @@
 from database.moderation_db import init_moderation_storage
 from .commands import moderation_cmd
-from .actions import ban_cmd, unban_cmd, mute_cmd, unmute_cmd, kick_cmd, promote_cmd, demote_cmd
+from .actions import ban_cmd, unban_cmd, mute_cmd, unmute_cmd, kick_cmd, promote_cmd, demote_cmd, tag_cmd, untag_cmd
 from .sudo import addsudo_cmd, rmsudo_cmd, sudolist_cmd
 
 init_moderation_storage()
